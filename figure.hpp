6cf89e0eda5261fcb23a9537c4245801c82047ba
Tiago Ferreira <tiago_caseiro_95@hotmail.com>
2022-06-04 23:07:32 +0100

    Rendering multiple spheres

--- figure.hpp
+++ figure.hpp
@@ -1,34 +1,54 @@
 #pragma once
 
 #include <optional>
+#include <tuple>
 
 #include "ray.hpp"
 
 class Figure {
   public:
-    Figure(const glm::vec3& _color) : color{_color} {}
-    virtual ~Figure()                                             = default;
-    virtual std::optional<float> intersects(const Ray& ray) const = 0;
+    using Hit = std::tuple<glm::vec3, glm::vec3, float, bool>;
 
-    const glm::vec3 color;
+    // Figure(const glm::vec3& _color) : color{_color} {}
+    Figure()                                                                            = default;
+    virtual ~Figure()                                                                   = default;
+    virtual std::optional<Hit> intersects(const Ray& ray, float tmin, float tmax) const = 0;
+
+    // const glm::vec3 color;
 };
 
 class Sphere : public Figure {
   public:
-    Sphere(const glm::vec3& _color, float _radius, const glm::vec3& _center)
-      : Figure(_color), radius{_radius}, center{_center} {}
+    Sphere(
+        // const glm::vec3& _color,
+        const glm::vec3& _center,
+        float _radius)
+      : Figure(), radius{_radius}, center{_center} {}
 
-    virtual std::optional<float> intersects(const Ray& ray) const override {
+    virtual std::optional<Hit> intersects(const Ray& ray, float tmin, float tmax) const override {
         auto ac           = ray.origin - center;
         auto a            = glm::dot(ray.direction, ray.direction);
         auto half_b       = glm::dot(ray.direction, ac);
         auto c            = glm::dot(ac, ac) - radius * radius;
         auto discriminant = half_b * half_b - a * c;
+
         if (discriminant < 0)
-            return {};
-        auto t1 = (-half_b + std::sqrt(discriminant)) / a;
-        auto t2 = (-half_b - std::sqrt(discriminant)) / a;
-        return {std::min(t1, t2)};
+            return std::nullopt;
+
+        auto t = (-half_b - std::sqrt(discriminant)) / a;
+        if (t < tmin || t > tmax) {
+            t = (-half_b + std::sqrt(discriminant)) / a;
+            if (t < tmin || t > tmax)
+                return std::nullopt;
+        }
+
+        auto point      = ray.at(t);
+        auto normal     = glm::normalize(point - center);
+        auto front_face = glm::dot(normal, ray.direction) < 0;
+        if (!front_face)
+            normal = -normal;
+
+        return {std::make_tuple(std::move(point), std::move(normal), t, front_face)};
     }
 
     const float radius;
--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include <fstream>
 #include <iostream>
+#include <memory>
+#include <vector>
 
 #include <glm/geometric.hpp>
 
 #include "figure.hpp"
 #include "ray.hpp"
+
+using World = std::vector<std::unique_ptr<Figure>>;
 
 // Image
 constexpr auto aspect_ratio = 16.f / 9.f;
@@ -16,10 +20,11 @@ constexpr auto viewport_height = 2.f;
 constexpr auto viewport_width  = viewport_height * aspect_ratio;
 constexpr auto focal_length    = 1.0f;
 
-auto origin            = glm::vec3();
-auto horizontal        = glm::vec3(viewport_width, 0, 0);
-auto vertical          = glm::vec3(0, viewport_height, 0);
-auto lower_left_corner = origin - horizontal / 2.f - vertical / 2.f - glm::vec3(0, 0, focal_length);
+constexpr auto origin     = glm::vec3();
+constexpr auto horizontal = glm::vec3(viewport_width, 0, 0);
+constexpr auto vertical   = glm::vec3(0, viewport_height, 0);
+constexpr auto lower_left_corner =
+    origin - horizontal / 2.f - vertical / 2.f - glm::vec3(0, 0, focal_length);
 
 auto calculateColor(const Ray& ray) {
     auto direction = glm::normalize(ray.direction);
@@ -33,21 +38,34 @@ void write(std::ostream& out, const glm::vec3& color) {
 }
 
 int main(int, char**) {
-    std::ofstream output{"./output.ppm"};
-    Sphere sphere{glm::vec3{1.0f, 0.0f, 0.0f}, 0.5f, glm::vec3{0.0f, 0.0f, -1.0f}};
+    constexpr auto tmin = std::numeric_limits<float>::min();
 
+    std::ofstream output{"./output.ppm"};
     output << "P3\n" << image_width << " " << image_height << "\n255\n";
+
+    World figures;
+    figures.push_back(std::make_unique<Sphere>(glm::vec3{0, 0, -1}, 0.5f));
+    figures.push_back(std::make_unique<Sphere>(glm::vec3{0, -100.5, -1}, 100.0f));
 
     for (auto i = image_height - 1; i >= 0; i--) {
         for (auto j = 0; j < image_width; j++) {
-            auto u = float(j) / (image_width - 1);
-            auto v = float(i) / (image_height - 1);
-            Ray ray(origin, lower_left_corner + u * horizontal + v * vertical - origin);
-            if (auto hit = sphere.intersects(ray)) {
-                auto t      = *hit;
-                auto p      = ray.at(t);
-                auto normal = glm::normalize(p - sphere.center);
-                auto color  = 0.5f * (normal + 1.0f);
+            auto u   = float(j) / (image_width - 1);
+            auto v   = float(i) / (image_height - 1);
+            auto ray = Ray(
+                origin, glm::normalize(lower_left_corner + u * horizontal + v * vertical - origin));
+            auto hit = std::optional<Figure::Hit>();
+
+            for (const auto& figure : figures) {
+                auto tmax = hit ? std::get<2>(*hit) : std::numeric_limits<float>::max();
+                if (auto temp = figure->intersects(ray, tmin, tmax)) {
+                    if (auto t = std::get<2>(*temp); t < tmax)
+                        hit = temp;
+                }
+            }
+
+            if (hit) {
+                const auto& normal = std::get<1>(*hit);
+                auto color         = 0.5f * (normal + 1.0f);
                 write(output, color);
             } else {
                 auto color = calculateColor(ray);
