f16bebbb9cfd081ada2f484cad7df2b2e9c3f526
Tiago Ferreira <tiago_caseiro_95@hotmail.com>
2022-06-05 00:32:22 +0100

    Utility functions

---
+++ utilities.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <iostream>
+#include <random>
+
+template <typename T>
+auto random() {
+    static std::uniform_real_distribution<T> distribution(0.0, 1.0);
+    static std::mt19937 generator;
+    return distribution(generator);
+}
+
+auto& operator<<(std::ostream& o, const glm::vec3& v) {
+    o << v.x << " " << v.y << " " << v.z;
+    return o;
+}
