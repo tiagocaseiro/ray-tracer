5e9b48ec8083b0bfada8cabde12af75cba87e02f
Tiago Ferreira <tiago_caseiro_95@hotmail.com>
2025-04-14 23:06:17 +0100

    Sphere class

--- src/figures.h
+++ src/figures.h
@@ -1,6 +1,20 @@
 #pragma once
 
-struct sphere
+#pragma warning(disable : 4514)
+
+#include "tuple.h"
+
+struct figure
 {
-    
+    figure() = default;
+};
+
+struct sphere : figure
+{
+    sphere(const tuple _centre, const float _radius) : figure(), centre(_centre), radius(_radius)
+    {
+    }
+
+    tuple centre;
+    float radius;
 };
