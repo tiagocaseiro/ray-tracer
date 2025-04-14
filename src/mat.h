91ae250f1a8ba0e64a2d6b12808bfc26022d68b9
Tiago Ferreira <tiago_caseiro_95@hotmail.com>
2025-04-14 23:06:10 +0100

    Formatting

--- src/mat.h
+++ src/mat.h
@@ -1,86 +1,85 @@
 #pragma once
 
 // Disable Spectre related warning
-#pragma warning(disable :5045)
+#pragma warning(disable : 5045)
 
-#include <span>
-#include <array>
 #include <algorithm>
+#include <array>
 #include <cmath>
+#include <span>
 
 #include "common.h"
+#include "ray.h"
 #include "tuple.h"
 
-template<size_t n>
+template <size_t n>
 struct mat
 {
-    explicit mat(const std::array<float,n*n>& _data = {}) : m_data(_data)
+    explicit mat(const std::array<float, n * n>& _data = {}) : m_data(_data)
     {
-    }
-    
-    [[nodiscard]] float& at(const size_t x, const size_t y)
-    {
-        return m_data[x*n+y];
     }
 
-    [[nodiscard]] float at(const size_t x, const size_t y) const 
+    [[nodiscard]] float& at(const size_t x, const size_t y)
     {
-        return m_data[x*n+y];
+        return m_data[x * n + y];
     }
-  
+
+    [[nodiscard]] float at(const size_t x, const size_t y) const
+    {
+        return m_data[x * n + y];
+    }
+
     static const mat<n>& identity()
-    {    
-        static const mat<n> id = std::invoke([]{
+    {
+        static const mat<n> id = std::invoke([] {
             mat<n> m;
-            for (size_t i = 0; i < n; i++)
+            for(size_t i = 0; i < n; i++)
             {
-                m.m_data[i*n+i] = 1;
-            } 
+                m.m_data[i * n + i] = 1;
+            }
             return m;
         });
         return id;
     }
 
 private:
-    std::array<float,n*n> m_data = {};
+    std::array<float, n * n> m_data = {};
 };
 
 using mat4 = mat<4>;
 using mat3 = mat<3>;
 using mat2 = mat<2>;
 
-template<size_t n>
+template <size_t n>
 std::ostream& operator<<(std::ostream& os, const mat<n>& mat)
 {
-    os << std::fixed << std::setprecision(2);
-
-    for (size_t i = 0; i != n; i++)
+    for(size_t i = 0; i != n; i++)
     {
-        for (size_t j = 0; j != n; j++)
+        for(size_t j = 0; j != n; j++)
         {
             os << mat.at(i, j) << " ";
         }
-        
+
         os << std::endl;
     }
 
     return os;
 }
 
-template<size_t n>
+template <size_t n>
 bool operator==(const mat<n>& a, const mat<n>& b)
 {
-    return (a != b) == false; 
+    return (a != b) == false;
 }
 
-template<size_t n>
+template <size_t n>
 bool operator!=(const mat<n>& a, const mat<n>& b)
 {
-    for (int i = 0; i != n; i++)
+    for(int i = 0; i != n; i++)
     {
-        for (int j = 0; j != n; j++)
+        for(int j = 0; j != n; j++)
         {
-            if (equals(a.at(i,j), b.at(i,j)) == false)
+            if(equals(a.at(i, j), b.at(i, j)) == false)
             {
                 return true;
             }
@@ -89,18 +88,18 @@ bool operator!=(const mat<n>& a, const mat<n>& b)
     return false;
 }
 
-template<size_t n>
+template <size_t n>
 mat<n> operator*(const mat<n>& a, const mat<n>& b)
 {
     mat<n> new_mat;
-    
-    for (size_t i = 0; i != n; i++)
+
+    for(size_t i = 0; i != n; i++)
     {
-        for (size_t j = 0; j != n; j++)
+        for(size_t j = 0; j != n; j++)
         {
-            for (size_t k = 0; k != n; k++)
+            for(size_t k = 0; k != n; k++)
             {
-                new_mat.at(i,j) += (a.at(i,k) * b.at(k, j));
+                new_mat.at(i, j) += (a.at(i, k) * b.at(k, j));
             }
         }
     }
@@ -108,15 +107,15 @@ mat<n> operator*(const mat<n>& a, const mat<n>& b)
     return new_mat;
 }
 
-template<size_t n>
+template <size_t n>
 mat<n> transpose(const mat<n>& m)
 {
     mat<n> new_m;
-    for (int i = 0; i != n; i++)
+    for(int i = 0; i != n; i++)
     {
-        for (int j = 0; j != n; j++)
+        for(int j = 0; j != n; j++)
         {
-            new_m.at(i,j) = m.at(j,i);
+            new_m.at(i, j) = m.at(j, i);
         }
     }
     return new_m;
@@ -124,54 +123,52 @@ mat<n> transpose(const mat<n>& m)
 
 tuple operator*(const mat4& m, const tuple& t)
 {
-    return tuple{
-        m.at(0,0)*t.x+m.at(0,1)*t.y+m.at(0,2)*t.z+m.at(0,3)*t.w,
-        m.at(1,0)*t.x+m.at(1,1)*t.y+m.at(1,2)*t.z+m.at(1,3)*t.w,
-        m.at(2,0)*t.x+m.at(2,1)*t.y+m.at(2,2)*t.z+m.at(2,3)*t.w,
-        m.at(3,0)*t.x+m.at(3,1)*t.y+m.at(3,2)*t.z+m.at(3,3)*t.w        
-    };
+    return tuple{m.at(0, 0) * t.x + m.at(0, 1) * t.y + m.at(0, 2) * t.z + m.at(0, 3) * t.w,
+                 m.at(1, 0) * t.x + m.at(1, 1) * t.y + m.at(1, 2) * t.z + m.at(1, 3) * t.w,
+                 m.at(2, 0) * t.x + m.at(2, 1) * t.y + m.at(2, 2) * t.z + m.at(2, 3) * t.w,
+                 m.at(3, 0) * t.x + m.at(3, 1) * t.y + m.at(3, 2) * t.z + m.at(3, 3) * t.w};
 }
 
-template<size_t n>
+template <size_t n>
 float determinant(const mat<n>& m)
 {
     float determinant = 0.f;
-    if constexpr (n == 2)
+    if constexpr(n == 2)
     {
-        determinant = m.at(0,0)*m.at(1,1) - m.at(0,1) * m.at(1,0);
+        determinant = m.at(0, 0) * m.at(1, 1) - m.at(0, 1) * m.at(1, 0);
     }
-    else{
-        for (int j = 0; j != n; j++)
+    else
+    {
+        for(int j = 0; j != n; j++)
         {
-            determinant+=(m.at(0,j)*cofactor(m, 0, j));
+            determinant += (m.at(0, j) * cofactor(m, 0, j));
         }
     }
-    
+
     return determinant;
 }
 
-
-template<size_t n>
-mat<n-1> submatrix(const mat<n>& m, const size_t x, const size_t y)
+template <size_t n>
+mat<n - 1> submatrix(const mat<n>& m, const size_t x, const size_t y)
 {
-    mat<n-1> sub;
+    mat<n - 1> sub;
 
-    auto m_i   = 0; 
-    for (auto sub_i = 0; sub_i != n-1; sub_i++)
+    auto m_i = 0;
+    for(auto sub_i = 0; sub_i != n - 1; sub_i++)
     {
-        if (m_i == x)
+        if(m_i == x)
         {
             m_i++;
         }
 
-        auto m_j   = 0; 
-        for (auto sub_j = 0; sub_j != n-1; sub_j++)
+        auto m_j = 0;
+        for(auto sub_j = 0; sub_j != n - 1; sub_j++)
         {
-            if (m_j == y)
+            if(m_j == y)
             {
                 m_j++;
             }
-            
+
             sub.at(sub_i, sub_j) = m.at(m_i, m_j);
             m_j++;
         }
@@ -186,12 +183,11 @@ float minor(const auto& m, const size_t x, const size_t y)
     return determinant(submatrix(m, x, y));
 }
 
-
 float cofactor(const auto& m, const size_t x, const size_t y)
 {
     auto cofactor = minor(m, x, y);
 
-    if ((x+y) % 2 != 0)
+    if((x + y) % 2 != 0)
     {
         cofactor *= -1;
     }
@@ -204,22 +200,22 @@ bool invertible(const auto& m)
     return determinant(m) != 0;
 }
 
-template<size_t n>
+template <size_t n>
 auto inverse(const mat<n>& m)
 {
     float d = determinant(m);
-    if (d == 0)
+    if(d == 0)
     {
         throw std::domain_error("Attempted to invert a non-invertible matrix");
     }
 
     mat<n> m_inverse;
-    for (int i = 0; i != n; i++)
+    for(int i = 0; i != n; i++)
     {
-       for (int j = 0; j != n; j++)
+        for(int j = 0; j != n; j++)
         {
             m_inverse.at(j, i) = cofactor(m, i, j) / d;
-        }     
+        }
     }
 
     return m_inverse;
@@ -227,35 +223,35 @@ auto inverse(const mat<n>& m)
 
 mat4 translate(const float x, const float y, const float z)
 {
-    auto mat = mat4::identity();
-    mat.at(0,3) = x;
-    mat.at(1,3) = y;
-    mat.at(2,3) = z;
+    auto mat     = mat4::identity();
+    mat.at(0, 3) = x;
+    mat.at(1, 3) = y;
+    mat.at(2, 3) = z;
     return mat;
 }
 
 mat4 scale(const float x, const float y, const float z)
 {
-    auto mat = mat4::identity();
-    mat.at(0,0) = x;
-    mat.at(1,1) = y;
-    mat.at(2,2) = z;
+    auto mat     = mat4::identity();
+    mat.at(0, 0) = x;
+    mat.at(1, 1) = y;
+    mat.at(2, 2) = z;
     return mat;
 }
 
 mat4 scale(const float s)
 {
-    return scale(s,s,s);
+    return scale(s, s, s);
 }
 
 mat4 rotate_x(const float angle_radians)
 {
     auto mat = mat4::identity();
-    
-    mat.at(1,1) = std::cos(angle_radians);
-    mat.at(1,2) = -std::sin(angle_radians);
-    mat.at(2,1) = std::sin(angle_radians);
-    mat.at(2,2) = std::cos(angle_radians);
+
+    mat.at(1, 1) = std::cos(angle_radians);
+    mat.at(1, 2) = -std::sin(angle_radians);
+    mat.at(2, 1) = std::sin(angle_radians);
+    mat.at(2, 2) = std::cos(angle_radians);
 
     return mat;
 }
@@ -263,11 +259,11 @@ mat4 rotate_x(const float angle_radians)
 mat4 rotate_y(const float angle_radians)
 {
     auto mat = mat4::identity();
-    
-    mat.at(0,0) = std::cos(angle_radians);
-    mat.at(0,2) = std::sin(angle_radians);
-    mat.at(2,0) = -std::sin(angle_radians);
-    mat.at(2,2) = std::cos(angle_radians);
+
+    mat.at(0, 0) = std::cos(angle_radians);
+    mat.at(0, 2) = std::sin(angle_radians);
+    mat.at(2, 0) = -std::sin(angle_radians);
+    mat.at(2, 2) = std::cos(angle_radians);
 
     return mat;
 }
@@ -275,35 +271,35 @@ mat4 rotate_y(const float angle_radians)
 mat4 rotate_z(const float angle_radians)
 {
     auto mat = mat4::identity();
-    
-    mat.at(0,0) = std::cos(angle_radians);
-    mat.at(0,1) = -std::sin(angle_radians);
-    mat.at(1,0) = std::sin(angle_radians);
-    mat.at(1,1) = std::cos(angle_radians);
+
+    mat.at(0, 0) = std::cos(angle_radians);
+    mat.at(0, 1) = -std::sin(angle_radians);
+    mat.at(1, 0) = std::sin(angle_radians);
+    mat.at(1, 1) = std::cos(angle_radians);
 
     return mat;
 }
 
 mat4 shear_x(const float y, const float z)
 {
-    auto mat = mat4::identity();
-    mat.at(0,1) = y;
-    mat.at(0,2) = z;
+    auto mat     = mat4::identity();
+    mat.at(0, 1) = y;
+    mat.at(0, 2) = z;
     return mat;
 }
 
 mat4 shear_y(const float x, const float z)
 {
-    auto mat = mat4::identity();
-    mat.at(1,0) = x;
-    mat.at(1,2) = z;
+    auto mat     = mat4::identity();
+    mat.at(1, 0) = x;
+    mat.at(1, 2) = z;
     return mat;
 }
 
 mat4 shear_z(const float x, const float y)
 {
-    auto mat = mat4::identity();
-    mat.at(2,0) = x;
-    mat.at(2,1) = y;
+    auto mat     = mat4::identity();
+    mat.at(2, 0) = x;
+    mat.at(2, 1) = y;
     return mat;
 }
--- tests/ray_test.cpp
+++ tests/ray_test.cpp
@@ -1,28 +1,129 @@
 #include <gtest/gtest.h>
 
-#include <sstream>
 #include <fstream>
+#include <sstream>
 
 #include "ray.h"
 
-TEST(ray, Ctor) 
+TEST(ray, Ctor)
 {
-  tuple origin    = make_point(1, 2, 3);
-  tuple direction = make_vector(4, 5, 6);
-  ray r = ray{origin, direction}; 
+    tuple origin    = make_point(1, 2, 3);
+    tuple direction = make_vector(4, 5, 6);
+    ray r           = ray{origin, direction};
 
-  EXPECT_EQ(r.origin, origin);
-  EXPECT_EQ(r.direction, direction);
+    EXPECT_EQ(r.origin, origin);
+    EXPECT_EQ(r.direction, direction);
 }
 
-TEST(ray, Position) 
+TEST(ray, Position)
 {
-  tuple origin    = make_point(2, 3, 4);
-  tuple direction = make_vector(1, 0, 0);
-  ray r = ray{origin, direction}; 
-  
-  EXPECT_EQ(position(r, 0.f), origin);
-  EXPECT_EQ(position(r, 1.f),  make_point(3, 3, 4));
-  EXPECT_EQ(position(r, -1.f), make_point(1, 3, 4));
-  EXPECT_EQ(position(r, 2.5f), make_point(4.5, 3, 4));
+    tuple origin    = make_point(2, 3, 4);
+    tuple direction = make_vector(1, 0, 0);
+    ray r           = ray{origin, direction};
+
+    EXPECT_EQ(position(r, 0.f), origin);
+    EXPECT_EQ(position(r, 1.f), make_point(3, 3, 4));
+    EXPECT_EQ(position(r, -1.f), make_point(1, 3, 4));
+    EXPECT_EQ(position(r, 2.5f), make_point(4.5, 3, 4));
+}
+
+TEST(ray, SphereIntersection)
+{
+    ray r    = ray{make_point(0, 0, -5), make_vector(0, 0, 1)};
+    sphere s = sphere{make_point(), 1.0f};
+
+    std::vector<intersection> intersections = intersects(r, s);
+
+    EXPECT_EQ(intersections.size(), 2);
+    EXPECT_EQ(intersections[0].t, 4.0f);
+    EXPECT_EQ(intersections[1].t, 6.0f);
+    EXPECT_EQ(intersections[0].figure, &s);
+    EXPECT_EQ(intersections[1].figure, &s);
+
+    r = ray{make_point(0, 1, -5), make_vector(0, 0, 1)};
+    s = sphere{make_point(), 1.0f};
+
+    intersections = intersects(r, s);
+
+    EXPECT_EQ(intersections.size(), 2);
+    EXPECT_EQ(intersections[0].t, 5.0f);
+    EXPECT_EQ(intersections[1].t, 5.0f);
+    EXPECT_EQ(intersections[0].figure, &s);
+    EXPECT_EQ(intersections[1].figure, &s);
+
+    r = ray{make_point(0, 2, -5), make_vector(0, 0, 1)};
+    s = sphere{make_point(), 1.0f};
+
+    intersections = intersects(r, s);
+
+    EXPECT_TRUE(intersections.empty());
+
+    r = ray{make_point(0, 0, 0), make_vector(0, 0, 1)};
+    s = sphere{make_point(), 1.0f};
+
+    intersections = intersects(r, s);
+
+    EXPECT_EQ(intersections.size(), 2);
+    EXPECT_EQ(intersections[0].t, -1.0f);
+    EXPECT_EQ(intersections[1].t, 1.0f);
+    EXPECT_EQ(intersections[0].figure, &s);
+    EXPECT_EQ(intersections[1].figure, &s);
+
+    r = ray{make_point(0, 0, 5), make_vector(0, 0, 1)};
+    s = sphere{make_point(), 1.0f};
+
+    intersections = intersects(r, s);
+
+    EXPECT_EQ(intersections.size(), 2);
+    EXPECT_EQ(intersections[0].t, -6.0f);
+    EXPECT_EQ(intersections[1].t, -4.0f);
+    EXPECT_EQ(intersections[0].figure, &s);
+    EXPECT_EQ(intersections[1].figure, &s);
+}
+
+TEST(ray, Intersection)
+{
+    sphere s = sphere{make_point(), 1.0f};
+
+    intersection inter = intersection{3.5, &s};
+
+    EXPECT_EQ(inter.t, 3.5);
+    EXPECT_EQ(inter.figure, &s);
+}
+
+TEST(ray, Hit)
+{
+    sphere s = sphere{make_point(), 1.0f};
+
+    intersection inter1 = {1, &s};
+    intersection inter2 = {2, &s};
+
+    std::optional<intersection> inter = hit({inter2, inter1});
+
+    EXPECT_TRUE(inter);
+    EXPECT_EQ(inter->t, 1);
+
+    inter1 = {-1, &s};
+    inter2 = {1, &s};
+
+    inter = hit({inter2, inter1});
+
+    EXPECT_TRUE(inter);
+    EXPECT_EQ(inter->t, 1);
+
+    inter1 = {-2, &s};
+    inter2 = {-1, &s};
+
+    inter = hit({inter2, inter1});
+
+    inter1 = {5, &s};
+    inter2 = {7, &s};
+
+    intersection inter3 = {3, &s};
+    intersection inter4 = {2, &s};
+
+    inter = hit({inter1, inter2, inter3, inter4});
+
+    EXPECT_TRUE(inter);
+    EXPECT_EQ(inter->t, 2);
 }
