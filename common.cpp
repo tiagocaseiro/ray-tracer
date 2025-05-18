234ed1315704ea16877585394684dc187704f042
Tiago Ferreira <tiago_caseiro_95@hotmail.com>
2025-05-19 00:09:50 +0100

    Common update

---
+++ common.cpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <iostream>
+#include <numbers>
+
+#include <glm/glm.hpp>
+
+glm::vec4 point(float x = 0.0f, float y = 0.0f, float z = 0.0f)
+{
+    return {x, y, z, 1.0f};
+}
+
+glm::vec4 vector(float x = 0.0f, float y = 0.0f, float z = 0.0f)
+{
+    return {x, y, z, 0.0f};
+}
+
+std::ostream& operator<<(std::ostream& os, const glm::vec4& v)
+{
+    os << "x: " << v.x;
+    os << " y: " << v.y;
+    os << " z: " << v.z;
+    os << " w: " << v.w;
+
+    return os;
+}
--- common.h
+++ common.h
@@ -7,22 +7,7 @@
 
 const float PI = std::numbers::pi;
 
-glm::vec4 point(float x = 0.0f, float y = 0.0f, float z = 0.0f)
-{
-    return {x, y, z, 1.0f};
-}
+glm::vec4 point(float x = 0.0f, float y = 0.0f, float z = 0.0f);
+glm::vec4 vector(float x = 0.0f, float y = 0.0f, float z = 0.0f);
 
-glm::vec4 vector(float x = 0.0f, float y = 0.0f, float z = 0.0f)
-{
-    return {x, y, z, 0.0f};
-}
-
-std::ostream& operator<<(std::ostream& os, const glm::vec4& v)
-{
-    os << "x: " << v.x;
-    os << " y: " << v.y;
-    os << " z: " << v.z;
-    os << " w: " << v.w;
-
-    return os;
-}
+std::ostream& operator<<(std::ostream& os, const glm::vec4& v);
